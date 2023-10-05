6d5d2aab8de213a029043406937207a93e948d8c
Faiz Ruzain <faiz.faiz8383@gmail.com>
2023-10-05 17:01:30 +0700

    lupa

--- src/main/java/com/afr/spring/boot/mycoolapp/rest/MyRestController.java
+++ src/main/java/com/afr/spring/boot/mycoolapp/rest/MyRestController.java
@@ -1,15 +1,36 @@
 package com.afr.spring.boot.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+
+  @Value("${first.name}")
+  private String fName;
+
+  @Value("${last.name}")
+  private String lName;
   
-  @GetMapping
+  @GetMapping("/")
   public String helloWorld() {
     return "HELLO mf WORLD!!!";
   }
+  
+  @GetMapping("/test")
+  public String test() {
+    return "restart";
+  }
 
+  @GetMapping("/fname")
+  public String getFirstName() {
+    return this.fName;
+  }
+
+  @GetMapping("/lname")
+  public String getLastName() {
+    return this.lName;
+  }
 
 }
